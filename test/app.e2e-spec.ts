14e098575e04335c8a9846ffc8bc739d4caf5354
Alexander Pompili <alexander.pompili@minnovi.com>
2022-02-23 15:38:15 +0100

    test: run test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,56 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should returns 200 "Ok"', () => {
+    return request(app.getHttpServer()).get('/').expect(200).expect('Ok');
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
+
+describe('RecordsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('should returns 200', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/')
+      .send({
+        startDate: new Date(),
+        endDate: new Date(),
+        minCount: 0,
+        maxCount: 1,
+      })
+      .expect(200);
+  });
+
+  it('should returns validation error 400', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        startDate: 'abc',
+        endDate: 'abc',
+        minCount: 'abc',
+        maxCount: 'abc',
+      })
+      .expect(400);
+  });
+
+  it('should returns validation error 400', () => {
+    return request(app.getHttpServer()).post('/').send({}).expect(400);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
